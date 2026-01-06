720863ed7a9bcbb9f812665cebbf6064b010deda
Michael Gonzalez <cyberalternative@gmail.com>
2026-01-06 08:35:54 +0100

    P10 - Constants + Feature extraction (fin)

---
+++ p10/src/constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+CANCER = "cancer"
+NORMAL = "normal"
+DATA_DIR = Path("../.data/mri_dataset_brain_cancer_oc")
+PREPARED_DIR = Path("../.data/prepared")
+FEATURES_DIR = Path("../.data/features")
+WITHOUT_LABEL = "sans_label"
+WITH_LABEL = "avec_labels"
